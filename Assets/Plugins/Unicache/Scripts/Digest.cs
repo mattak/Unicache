4aa15c9f7ce7ab6389a2df79d9ac95c48b3539d6
mattak <mattak.me@gmail.com>
2017-01-10 01:32:16 +0900

    issues/9 feat(Digest): create sha1 digest

---
+++ Assets/Plugins/Unicache/Scripts/Digest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Unicache
+{
+    public static class Digest
+    {
+        public static string SHA1(string message)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] request = System.Text.Encoding.UTF8.GetBytes(message);
+            byte[] result = sha.ComputeHash(request);
+            return BitConverter.ToString(result).ToLower().Replace("-", "");
+        }
+    }
+}
