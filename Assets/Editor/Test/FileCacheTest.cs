a45fb7bd9eb3ebff13eaacd0ae33010b164ab2e9
mattak <mattak.me@gmail.com>
2017-01-10 00:10:26 +0900

    issues/7 test(FileCache): unit test

---
+++ Assets/Editor/Test/FileCacheTest.cs
@@ -0,0 +1,120 @@
+﻿using System.IO;
+using NUnit.Framework;
+using UniRx;
+
+namespace Unicache.Test
+{
+    public class FileCacheTest
+    {
+        private IUnicache cache;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.cache = new FileCacheForTest();
+            this.cache.ClearAll();
+        }
+
+        public void TearDown()
+        {
+            this.cache.ClearAll();
+        }
+
+        [Test]
+        public void HasCacheTest()
+        {
+            var path = UnicacheConfig.Directory + "foo";
+
+            Assert.IsFalse(this.cache.HasCache("foo"));
+
+            IO.Write(path, new byte[] {0x01});
+
+            Assert.AreEqual(
+                IO.Read(path),
+                new byte[] {0x01}
+            );
+
+            Assert.IsTrue(this.cache.HasCache("foo"));
+        }
+
+        [Test]
+        public void GetCacheTest()
+        {
+            var path = UnicacheConfig.Directory + "foo";
+
+            Assert.Throws<FileNotFoundException>(() => { this.cache.GetCache("foo"); });
+
+            IO.MakeParentDirectory(path);
+            IO.Write(path, new byte[] {0x01});
+
+            Assert.AreEqual(
+                this.cache.GetCache("foo"),
+                new byte[] {0x01}
+            );
+        }
+
+        [Test]
+        public void SetCacheTest()
+        {
+            Assert.IsFalse(this.cache.HasCache("foo"));
+
+            this.cache.SetCache("foo", new byte[] {0x01});
+            Assert.IsTrue(this.cache.HasCache("foo"));
+            Assert.AreEqual(this.cache.GetCache("foo"), new byte[] {0x01});
+        }
+
+        [Test]
+        public void FetchTest()
+        {
+            this.cache.Locator = new TestCacheLocator();
+            this.cache.Handler = new TestCacheHandler();
+
+            int count = 0;
+            Assert.IsFalse(this.cache.HasCache("url"));
+
+            this.cache.Fetch("url")
+                .Subscribe(data =>
+                {
+                    count++;
+                    Assert.AreEqual(data, new byte[] {0x01});
+                });
+
+            Assert.IsTrue(this.cache.HasCache("url"));
+            Assert.AreEqual(count, 1);
+
+            this.cache.Fetch("url")
+                .Subscribe(data =>
+                {
+                    count++;
+                    Assert.AreEqual(data, new byte[] {0x01});
+                });
+
+            Assert.IsTrue(this.cache.HasCache("url"));
+            Assert.AreEqual(count, 2);
+        }
+
+        class FileCacheForTest : FileCache
+        {
+            protected override IObservable<byte[]> AsAsync(IObservable<byte[]> observable)
+            {
+                return observable;
+            }
+        }
+
+        class TestCacheLocator : ICacheLocator
+        {
+            public string CreatePath(string url)
+            {
+                return url;
+            }
+        }
+
+        class TestCacheHandler : ICacheHandler
+        {
+            public IObservable<byte[]> Fetch(string url)
+            {
+                return Observable.Return(new byte[] {0x01});
+            }
+        }
+    }
+}
