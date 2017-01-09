8bfc858d8b4a5fb617f4a63159ca84dbeea3c33d
mattak <mattak.me@gmail.com>
2017-01-10 01:33:07 +0900

    issues/9 refactor(SimpleCacheLocator): export digest function

--- Assets/Plugins/Unicache/Scripts/Plugin/SimpleCacheLocator.cs
+++ Assets/Plugins/Unicache/Scripts/Plugin/SimpleCacheLocator.cs
@@ -1,21 +1,10 @@
-using System;
-using System.Security.Cryptography;
-
 namespace Unicache.Plugin
 {
     public class SimpleCacheLocator : ICacheLocator
     {
         public string CreatePath(string url)
         {
-            return this.CreateHash(url);
-        }
-
-        private string CreateHash(string url)
-        {
-            SHA1 sha = new SHA1CryptoServiceProvider();
-            byte[] request = System.Text.Encoding.UTF8.GetBytes(url);
-            byte[] result = sha.ComputeHash(request);
-            return BitConverter.ToString(result).ToLower().Replace("-", "");
+            return Digest.SHA1(url);
         }
     }
 }
