bb15c6b561a1ae4b0daad676af166ab2341b478a
mattak <mattak.me@gmail.com>
2017-01-14 14:58:07 +0900

    issues/9 feat(Example/Versioning): implement version cache example

---
+++ Assets/Plugins/Unicache/Examples/Versioning/VersionCacheLocator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unicache;
+
+namespace UnicacheExample.Version
+{
+    public class VersionCacheLocator : ICacheLocator
+    {
+        public IDictionary<string, string> VersionMap { get; set; }
+
+        public VersionCacheLocator(IDictionary<string, string> versionMap)
+        {
+            this.VersionMap = versionMap;
+        }
+
+        // save to: <file>/<version>
+        public string CreateCachePath(string key)
+        {
+            var keyhash = Digest.SHA1(key);
+            var version = this.VersionMap[key];
+            return string.Format("{0}/{1}", keyhash, version);
+        }
+
+        public IEnumerable<string> GetSameKeyCachePathes(string key, IEnumerable<string> cachePathes)
+        {
+            var keyhash = Digest.SHA1(key);
+            return cachePathes.Where(path => path.StartsWith(keyhash));
+        }
+    }
+}
---
+++ Assets/Plugins/Unicache/Examples/Versioning/VersionUrlLocator.cs
@@ -0,0 +1,13 @@
+﻿using Unicache;
+
+namespace UnicacheExample.Version
+{
+    public class VersionUrlLocator : IUrlLocator
+    {
+        public string CreateUrl(string key)
+        {
+            var url = string.Format("https://raw.githubusercontent.com/mattak/Unicache/master/art/{0}.jpg", key);
+            return url;
+        }
+    }
+}
