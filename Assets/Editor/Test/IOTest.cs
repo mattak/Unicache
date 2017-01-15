d5b78687a5b1feeff000d1fa04cecf1fef0e7765
mattak <mattak.me@gmail.com>
2017-01-15 17:50:43 +0900

    issues/3 feat(IO): create RecursiveListFiles

---
+++ Assets/Editor/Test/IOTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Unicache.Test
+{
+    public class IOTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            if (Directory.Exists("/tmp/unicache_test"))
+            {
+                Directory.Delete("/tmp/unicache_test", true);
+            }
+        }
+
+        [Test]
+        public void RecursiveListFilesTest()
+        {
+            IO.MakeDirectory("/tmp/unicache_test/a/b");
+            IO.Write("/tmp/unicache_test/a/b1", new byte[] {0x01});
+            IO.Write("/tmp/unicache_test/a/b/c1", new byte[] {0x01});
+            IO.Write("/tmp/unicache_test/a/b/c2", new byte[] {0x01});
+
+            Assert.IsTrue(File.Exists("/tmp/unicache_test/a/b1"));
+            Assert.IsTrue(File.Exists("/tmp/unicache_test/a/b/c1"));
+            Assert.IsTrue(File.Exists("/tmp/unicache_test/a/b/c2"));
+
+            var result = IO.RecursiveListFiles("/tmp/unicache_test").ToList();
+            result.Sort();
+
+            Assert.AreEqual(new[]
+            {
+                "/tmp/unicache_test/a/b/c1",
+                "/tmp/unicache_test/a/b/c2",
+                "/tmp/unicache_test/a/b1",
+            }, result.ToArray());
+        }
+    }
+}
