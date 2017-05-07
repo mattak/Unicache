6f7772f94407f145ce73a910adf9c86b6fd4da40
mattak <mattak.me@gmail.com>
2017-05-07 19:47:10 +0900

    issues/20 feat(Encoder/Decoder): define {Void,Xor}EncoderDecoder

---
+++ Assets/Plugins/Unicache/Scripts/Plugin/VoidEncoderDecoder.cs
@@ -0,0 +1,15 @@
+﻿namespace Unicache.Plugin
+{
+    public class VoidEncoderDecoder : ICacheEncoderDecoder
+    {
+        public byte[] Decode(byte[] data)
+        {
+            return data;
+        }
+
+        public byte[] Encode(byte[] data)
+        {
+            return data;
+        }
+    }
+}
